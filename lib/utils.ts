c2a1da7e9ba855614bc6002a65bf56f3ab3b9e67
Shourya <shourya0864@gmail.com>
2025-07-11 02:14:44 +0530

    changed the library id

--- lib/utils.ts
+++ lib/utils.ts
@@ -301,7 +301,7 @@ export function daysAgo(inputDate: Date): string {
 }
 
 export const createIframeLink = (videoId: string) =>
-  `https://iframe.mediadelivery.net/embed/448591/${videoId}?autoplay=true&preload=true`;
+  `https://iframe.mediadelivery.net/embed/465916/${videoId}?autoplay=true&preload=true`;
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const doesTitleMatch = (videos: any, searchQuery: string) =>
